25a599ba847c29c0f8d6d1570e2a56b437d6f08b
Elias Papavasileiou <eliaspap@protonmail.com>
2018-09-08 03:38:00 +0300

    Fix #19679: Collapsible FAQ items

--- docs/conf.py
+++ docs/conf.py
@@ -135,6 +135,7 @@ html_favicon = 'favicon.png'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_extra_path = ['faq.html']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
